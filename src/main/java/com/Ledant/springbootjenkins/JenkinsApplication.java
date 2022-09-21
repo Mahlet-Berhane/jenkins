a120c8bd9ae6facb8c0b22efb11afe774b050d13
Mahlet Berhane <mahlet.berhane2@gmail.com>
2022-09-21 15:49:03 -0500

    First commit

---
+++ src/main/java/com/Ledant/springbootjenkins/JenkinsApplication.java
@@ -0,0 +1,27 @@
+package com.Ledant.springbootjenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinsApplication.class);
+
+	@PostConstruct
+	public void intt(){
+		logger.info("Application started.............");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application has executed.....");
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+
+}
